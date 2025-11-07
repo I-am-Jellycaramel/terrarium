f10618b2eee03689fa2555138c1844201ed0965c
Jellycaramel <129017246+I-am-Jellycaramel@users.noreply.github.com>
2025-11-07 12:52:36 +0900

    코드 개선: 반복되는 부분 반복문으로 묶음

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
 let maxZIndex = 14
 
-dragElement(document.getElementById('plant1'));
-dragElement(document.getElementById('plant1'));
-dragElement(document.getElementById('plant2'));
-dragElement(document.getElementById('plant3'));
-dragElement(document.getElementById('plant4'));
-dragElement(document.getElementById('plant5'));
-dragElement(document.getElementById('plant6'));
-dragElement(document.getElementById('plant7'));
-dragElement(document.getElementById('plant8'));
-dragElement(document.getElementById('plant9'));
-dragElement(document.getElementById('plant10'));
-dragElement(document.getElementById('plant11'));
-dragElement(document.getElementById('plant12'));
-dragElement(document.getElementById('plant13'));
-dragElement(document.getElementById('plant14'));
+for (let i = 1; i < 15; i++) {
+    dragElement(document.getElementById(`plant${i}`));
+}
 
 function dragElement(terrariumElement) {
     // 이미지일 때만
